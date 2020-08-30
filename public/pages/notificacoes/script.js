1a356afc816b95f1965d44dbef023eca18124351
Wellington.Ramos <wellingtom1234520@gmail.com>
2020-08-29 23:45:41 -0300

    Organização de codigo das tarefas #6 #7

--- public/pages/notificacoes/script.js
+++ public/pages/notificacoes/script.js
@@ -2,22 +2,9 @@ $(document).ready(async () => {
     //#region Vuedata
     dataVue.UsuarioContexto.NIVEL_USUARIO = await GetSessaoPHP(SESSOESPHP.NIVEL_USUARIO);
     await app.$set(dataVue, 'Tabs', { Notificacao: true, Propostas: false });
-    await app.$set(dataVue, 'TabNCategorias', GetTipoMensagem());
-    await app.$set(dataVue, "TabNPagina", 1);
-    await app.$set(dataVue, "TabNListCarregando", false);
-    await app.$set(dataVue, "TabNCarregando", true);
-
-    await app.$set(dataVue, "TabNList", []);
-    await app.$set(dataVue, "TabNPageController", { paginas: 1, pagina_Atual: 1 })
     //#endregion
 
-    await BuscaListaNotificacoes();
-    await app.$watch("dataVue.TabNPageController", async function (a, o) {
-        await BuscaListaNotificacoes();
-    }, { deep: true });
-    await app.$watch("dataVue.TabNCategorias", async function (a, o) {
-        await BuscaListaNotificacoes();
-    }, { immediate:true,deep: true });
+ 
     document.addEventListener("BuscaNotificacao", async () => {
         await BuscaListaNotificacoes();
         await BuscaPropostas();
@@ -27,79 +14,7 @@ $(document).ready(async () => {
 
 
 
-    //#region TAB PROPOSTA
-    if (dataVue.UsuarioContexto.NIVEL_USUARIO == 0) {
-
-        app.$set(dataVue, "TabPFiltro", { Projeto: null });
-        //#region Seletor
-        var ProjetoSeletor = () => {
-            return {
-                id: 'ProjetosSeletor',
-                visivel: () => { return true },
-                titulo: 'Filtrar por projeto',
-                disabled: () => { return false },
-                entidade: "TabPFiltro",
-                campo: "Projeto",
-                limpavel: true,
-                icone: false,
-                obrigatorio: false,
-                ajax: async (ss) => {
-                    return await new Promise(async resolve => {
-
-                        var saida = await WMExecutaAjax("ProjetoBO", "GETPROJETOSPORUSUARIOCONTEXTO");
-                        saida.map(item => {
-                            return {
-                                id: item.id,
-                                nome: item.nome
-                            }
-                        });
-                        resolve(saida);
-
-                    });
-                }
-            };
-        };
-        await app.$set(dataVue, "ProjetoSeletor", ProjetoSeletor());
-
-        //#endregion
-
-        app.$watch("dataVue.TabPFiltro", async (n, o) => {
-            await BuscaPropostas();
-        }, { immediate: true, deep: true });
-
-
-        app.$set(dataVue, "PropostasCarregando", true);
-        app.$set(dataVue, "Propostas", { listaP: [], listaN: [] });
-
-        await BuscaPropostas();
-
-        app.$set(dataVue, "CancelaProposta", async (idProposta) => {
-            await BloquearTela()
-            $cancelou = await WMExecutaAjax("PropostaBO", "RECUSARPROPOSTA", { IDPROPOSTA: idProposta });
-            await BuscaPropostas();
-            MostraMensagem("Proposta cancelada com sucesso.", ToastType.SUCCESS, "Sucesso");
-            if (app.dataVue.Propostas.listaP.length == 0 && app.dataVue.Propostas.listaN.length == 0 && app, dataVue.TabPFiltro.Projeto != null)
-                app.$refs.SeletorFiltra.$refs.ProjetosSeletor.clearSelection();
-            await DesbloquearTela();
-        });
-        app.$set(dataVue, "AprovaProposta", async (idProposta) => {
-            await BloquearTela();
-            $AProvou = await WMExecutaAjax("PropostaBO", "APROVARPROPOSTA", { IDPROPOSTA: idProposta });
-            await BuscaPropostas();
-            MostraMensagem("Proposta aprovada com sucesso.", ToastType.SUCCESS, "Sucesso");
-            if (app.dataVue.Propostas.listaP.length == 0 && app.dataVue.Propostas.listaN.length == 0 && app, dataVue.TabPFiltro.Projeto != null)
-                app.$refs.SeletorFiltra.$refs.ProjetosSeletor.clearSelection();
-            await DesbloquearTela();
-        });
-
-
-
-    }
-    //TODO TRAZER AS PROPOSTAS AI BELE WELL DO FUTURO
-    // await WMExecutaAjax("PropostaBO","BuscaPropostasPendentesClientes")
-    //{listaP: Array(1), listaN: Array(0)}
-    //#endregion
-
+ 
 });
 
 const GetTipoMensagem = () => {
